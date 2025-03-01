80dcb79b72bfc760d5442256e441c397b0da6d43
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-03-01 19:02:50 +0000

    Move camera instance to Simulation class

--- src/simulation/simulation.hpp
+++ src/simulation/simulation.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
 #include <Rendering/Window/Window.hpp>
+#include <Camera/Camera.hpp>
+
+#include <glm/glm.hpp>
+
+using namespace glm;
 
 class Simulation {
     private:
         Window window{WIDTH, HEIGHT, "Solar System Simulation"};
+        Camera camera{WIDTH, HEIGHT, vec3(0.0f, 0.0f, 2.0f)};
     public:
-        static const int WIDTH = 1000;
+        static const int WIDTH = 1920;
         static const int HEIGHT = 1000;
 
         void Run();
